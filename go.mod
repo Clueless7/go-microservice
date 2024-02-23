945748d42c3c13c81cd6eedee46839cbbe37f1c8
Josel Catalan <catalanjosel123@gmail.com>
2024-02-23 19:44:15 +0800

    task: added chi router and middleware

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Clueless7/go-microservice
 
 go 1.21.4
 
-require github.com/go-chi/chi v1.5.5
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.12
+)
--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
@@ -20,13 +26,5 @@ func main() {
 }
 
 func basicHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		if r.URL.Path == "/foo" {
-			w.Write([]byte("foo!"))
-
-			return
-		}
-	}
-
 	w.Write([]byte("Hello, World"))
 }
